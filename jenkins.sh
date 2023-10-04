4e43a11ba442f26ddfc310b71608630c9d4d0163
Garvit Kulshrestha <garvitindian@gmail.com>
2023-10-04 13:27:41 +0100

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo dnf install java-11-amazon-corretto -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
