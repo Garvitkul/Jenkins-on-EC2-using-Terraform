f7e7eeeae46c2c24c96766a791d1420ef41b4d02
Garvit Kulshrestha <garvitindian@gmail.com>
2023-10-04 13:27:16 +0100

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,19 @@
+#define your region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "172.31.0.0/16"
+}
